15e395a63d5e3a8b811524ea31d5795a77466b99
Half-Savant <47034428+Half-Savant@users.noreply.github.com>
2020-02-24 18:31:15 +0100

    Add files via upload
    fix

--- Clock.java
+++ Clock.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.ArrayList;
+//import java.util.Collections;
 
 public class Clock {
 //    private static void zegar(int a, int b){
