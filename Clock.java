bc2bdb48b1e6aac8b2cc8db8dcac32159e976272
Half-Savant <47034428+Half-Savant@users.noreply.github.com>
2020-02-24 18:30:00 +0100

    Add files via upload
    final commit(I hope)

---
+++ Clock.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Clock {
+//    private static void zegar(int a, int b){
+//        int zegarek = 0;
+//        for(int i = 0; i < 24; i++){
+//            for(int j = 0; j < 60; j++) {
+//                zegarek++;
+//            }
+//        }
+//        System.out.println(zegarek);
+//    }
+//źle zrozumiałem zadanie dlatego zakomentowane
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        int c = 5;
+        int d = 9;
+        zegar(a, b, c, d);
+    }
+
+    private static void zegar(int i, int j, int k, int l){
+        if(i <= 2){
+            if(i == 2){
+                if(j <= 3){
+                    if(k <= 5){
+                        if(l <= 9){
+                            //ArrayList<Integer> time = new ArrayList<>();
+                            //time.add(i);
+                            //time.add(j);
+                            //time.add(k);
+                            //time.add(l);
+                            String overclock = String.valueOf(i) + String.valueOf(j) + ":" + String.valueOf(k) + String.valueOf(l);
+                            System.out.println(overclock);
+                            //Colletions.sort(time, Collections.reverseOrder());
+                        }
+                        else {
+                            System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                        }
+                    }
+                    else {
+                        System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                    }
+                }
+                else {
+                    System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                }
+            }
+            else{
+                if(j <= 9){
+                    if(k <= 5){
+                        if(l <= 9){
+                            //ArrayList<Integer> time = new ArrayList<>();
+                            //time.add(i);
+                            //time.add(j);
+                            //time.add(k);
+                            //time.add(l);
+                            String overclock = String.valueOf(i) + String.valueOf(j) + ":" + String.valueOf(k) + String.valueOf(l);
+                            System.out.println(overclock);
+                            //Colletions.sort(time, Collections.reverseOrder());
+                        }
+                        else {
+                            System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                        }
+                    }
+                    else {
+                        System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                    }
+                }
+                else {
+                    System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+                }
+            }
+        }
+        else {
+            System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+        }
+    }
+}
