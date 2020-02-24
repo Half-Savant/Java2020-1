4083bc359087655c944da4827f01c3ec54c9bdd4
Half-Savant <47034428+Half-Savant@users.noreply.github.com>
2020-02-24 19:09:56 +0100

    Add files via upload
    prawie

--- Clock.java
+++ Clock.java
@@ -1,17 +1,7 @@
-//import java.util.ArrayList;
-//import java.util.Collections;
-
+import java.util.ArrayList;
+import java.util.Collections;
+//Program tworzy z 4 dostępnych cyfr najpóźniejszą możliwą godzinę.
 public class Clock {
-//    private static void zegar(int a, int b){
-//        int zegarek = 0;
-//        for(int i = 0; i < 24; i++){
-//            for(int j = 0; j < 60; j++) {
-//                zegarek++;
-//            }
-//        }
-//        System.out.println(zegarek);
-//    }
-//źle zrozumiałem zadanie dlatego zakomentowane
     public static void main(String[] args){
         int a = 2;
         int b = 3;
@@ -19,21 +9,69 @@ public class Clock {
         int d = 9;
         zegar(a, b, c, d);
     }
-
+    private static void maks(int i, int j, int k, int l){
+        if(i >= j){
+            if(i >= k){
+                if(i >= l){
+                    int biggest = i;
+                }
+                else{
+                    int biggest = l;
+                }
+            }
+            else if(k >= l){
+                int biggest = k;
+            }
+            else {
+                int biggest = l;
+            }
+        }
+        else if(j >= k){
+            if(j >= l){
+                int biggest = j;
+            }
+            else {
+                int biggest = l;
+            }
+        } else if(k >= l){
+            int biggest = k;
+        } else {
+            int biggest = l;
+        }
+        if(i == 2){
+            int first = i;
+        } else if(j == 2){
+            int first = j;
+        } else if(k == 2){
+            int first = k;
+        } else if(l == 2){
+            int first = l;
+        } else if(i == 1){
+            int first = i;
+        } else if(j == 1){
+            int first = j;
+        } else if(k == 1){
+            int first = k;
+        } else if(l == 1){
+            int first = l;
+        } else {
+            System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
+        }
+    }
     private static void zegar(int i, int j, int k, int l){
         if(i <= 2){
             if(i == 2){
                 if(j <= 3){
                     if(k <= 5){
                         if(l <= 9){
-                            //ArrayList<Integer> time = new ArrayList<>();
-                            //time.add(i);
-                            //time.add(j);
-                            //time.add(k);
-                            //time.add(l);
+                            ArrayList<Integer> time = new ArrayList<>();
+                            time.add(i);
+                            time.add(j);
+                            time.add(k);
+                            time.add(l);
                             String overclock = String.valueOf(i) + String.valueOf(j) + ":" + String.valueOf(k) + String.valueOf(l);
                             System.out.println(overclock);
-                            //Colletions.sort(time, Collections.reverseOrder());
+                            Collections.sort(time, Collections.reverseOrder());
                         }
                         else {
                             System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
@@ -51,14 +89,14 @@ public class Clock {
                 if(j <= 9){
                     if(k <= 5){
                         if(l <= 9){
-                            //ArrayList<Integer> time = new ArrayList<>();
-                            //time.add(i);
-                            //time.add(j);
-                            //time.add(k);
-                            //time.add(l);
+                            ArrayList<Integer> time = new ArrayList<>();
+                            time.add(i);
+                            time.add(j);
+                            time.add(k);
+                            time.add(l);
                             String overclock = String.valueOf(i) + String.valueOf(j) + ":" + String.valueOf(k) + String.valueOf(l);
                             System.out.println(overclock);
-                            //Colletions.sort(time, Collections.reverseOrder());
+                            Collections.sort(time, Collections.reverseOrder());
                         }
                         else {
                             System.out.println("Podany czas nie jest poprawny ze standardami ziemskimi!");
